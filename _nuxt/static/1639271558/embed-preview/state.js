fc3add7952ad3612fc200c702e6524260ef9a1bc
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-12-12 05:14:18 +0000

    deploy: 4089ca2e5fe9e2d8ed5d656bf3843ead174d6301

--- _nuxt/static/1639271558/embed-preview/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639271558",layout:"embed",error:a,serverRendered:true,routePath:"\u002Fembed-preview",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ef2a8f33"}}}}(null));
