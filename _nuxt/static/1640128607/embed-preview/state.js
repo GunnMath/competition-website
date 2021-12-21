ee23248cc03f242c3476b07088f468728a106b48
ImNotRog <ImNotRog@users.noreply.github.com>
2021-12-21 23:22:49 +0000

    deploy: cc57c82560f767c6700596b4680e120429e87d24

--- _nuxt/static/1640128607/embed-preview/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640128607",layout:"embed",error:a,serverRendered:true,routePath:"\u002Fembed-preview",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a26abdb8"}}}}(null));
