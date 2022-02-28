08cea86efb16c545e24d6611c654e2a44c0c8a94
ImNotRog <ImNotRog@users.noreply.github.com>
2022-02-28 07:24:58 +0000

    deploy: 31529fba9f9af24b803a0c106d229d349bbee2e9

---
+++ _nuxt/static/1646033097/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646033097",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"735e4e76"}}}}(null,"\u002F"));
