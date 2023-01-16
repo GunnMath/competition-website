1ab01ff61836c8d91f5d187f410a6a334aa11a9d
AlanReads <AlanReads@users.noreply.github.com>
2023-01-16 22:02:35 +0000

    deploy: 52951f0d1c72cbc9b31670cf5764bd8ebab2cadd

--- _nuxt/static/1673906112/embed-preview/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673906112",layout:"embed",error:a,serverRendered:true,routePath:"\u002Fembed-preview",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0636f6cf"}}}}(null));
---
+++ _nuxt/static/1673906553/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673906553",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5047b3c4"}}}}(null,"\u002F"));
